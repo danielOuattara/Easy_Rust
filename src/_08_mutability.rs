6918a3d79c1a94601e6386a6cf77ecdef4784152
danielOuattara <daniel.ouattara@gmx.com>
2023-08-01 22:46:28 +0200

    08 mutability

---
+++ src/_08_mutability.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // let my_number = 8;
+    // my_number = 10; // ⚠️ Error
+
+    let mut my_number = 8;
+    my_number = 10;
+    println!("{my_number}");
+
+    // let mut my_variable = 8; // it is now an i32. That can't be changed
+    // my_variable = "Hello, world!"; // ⚠️
+}
